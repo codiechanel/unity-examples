99b479e619bc9a640bd2c7a67482904bd4eb0f10
codiechanel <codiechanel@gmail.com>
2018-11-05 08:08:10 +0800

    cube material

--- custom inspector/Assets/Cube.cs
+++ custom inspector/Assets/Cube.cs
@@ -12,7 +12,12 @@ public class Cube : MonoBehaviour
 
     public void GenerateColor()
     {
+        // this is referencing the shared material
+        // so all objects even if its not a cube will get colorized...
         GetComponent<Renderer>().sharedMaterial.color = Random.ColorHSV();
+        //Instantiating material due to calling renderer.material during edit mode. 
+        //This will leak materials into the scene.You most likely want to use renderer.sharedMaterial instead.
+        //GetComponent<Renderer>().material.color = Random.ColorHSV();
     }
 
     public void Reset()
